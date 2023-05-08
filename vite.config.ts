3b443893665b73aa7c76d2669d8c3ef98005db6a
Ardanas <ardans@foxmail.com>
2023-05-08 21:28:14 +0800

    feat: add test

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import Vue from '@vitejs/plugin-vue';
 import UnoCSS from 'unocss/vite';
@@ -39,4 +40,11 @@ export default defineConfig({
       },
     }),
   ],
+  test: {
+    include: ['test/**/*.test.ts'],
+    environment: 'jsdom',
+    deps: {
+      inline: ['@vue'],
+    },
+  },
 });
