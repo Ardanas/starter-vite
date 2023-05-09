a663844bee876c1afcf1fea4d7577f2fe148eb82
Ardanas <ardans@foxmail.com>
2023-05-09 15:42:26 +0800

    feat: add icon

--- uno.config.ts
+++ uno.config.ts
@@ -1,12 +1,16 @@
 // uno.config.ts
-import { defineConfig } from 'unocss';
-import { presetUno, presetAttributify } from 'unocss';
+import { defineConfig } from 'unocss'
+import { presetAttributify, presetIcons, presetUno } from 'unocss'
 
 export default defineConfig({
   presets: [
     presetUno({
       dark: 'media',
     }),
+    presetIcons({
+      scale: 1.2,
+      warn: true,
+    }),
     presetAttributify(),
   ],
-});
+})
